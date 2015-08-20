69425d59fa4d8e38e31d5cfd302ad5cd4ad30a4d
esoftkz <2841496@gmail.com>
2015-08-21 01:26:08 +0600

    Обновил файлы

--- views/yii2-app/layouts/left.php
+++ views/yii2-app/layouts/left.php
@@ -1,26 +1,12 @@
 <?php
-use yii\bootstrap\Nav;
 use esoftkz\adminlte\widgets\drmenu\Drmenu;
-
-$items = [
-	['label' => 'Пользователи', 'url' => '/cms/user', 'icon_class' => 'fa-users'],
-	['label' => 'Локализация', 'icon_class' => 'fa-globe', 'items' => [
-			['label' => 'Языки', 'url' => '/cms/language', 'icon_class' => 'fa-flag'],
-			['label' => 'Переводы', 'url' => '/cms/translation', 'icon_class' => 'fa-bookmark'],
-		]
-	],
-	['label' => 'СМС', 'icon_class' => 'fa-share', 'items' => [
-			['label' => 'Страницы', 'url' => '/cms/post', 'icon_class' => 'fa-circle-o'],
-			['label' => 'SEO', 'url' => '/cms/meta', 'icon_class' => 'fa-circle-o'],
-		]
-	],	
-]; 
 
 ?>
 <aside class="main-sidebar">
 
     <section class="sidebar">
-		<?= Drmenu::widget(['items' => $items]) ?>
+		<?= Drmenu::widget(['items' => Yii::$app->params['menu_items']]) ?>
     </section>
 
 </aside>
+
